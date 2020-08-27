fb74cb959e6ccb905d71e0d3aabd0bd901e55963
Alex Wu <alex@blitzm.com>
2020-08-27 19:41:31 +1000

    add scheduler

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
 require('dotenv').config();
 const crawler = require('./crawler');
 const emailer = require('./emailer');
+const schedule = require('node-schedule');
 
 async function run() {
-    const posts = await crawler.getNewPosts();
-    emailer.sendEmailByGmail(posts);
+    
+    // run at 6 am every day 
+    schedule.scheduleJob('* * 6 * * *', async function(){
+        const posts = await crawler.getNewPosts();
+        emailer.sendEmailByGmail(posts);
+    });
+
+    // run at 6 pm every day 
+    schedule.scheduleJob('* * 18 * * *', async function(){
+        const posts = await crawler.getNewPosts();
+        emailer.sendEmailByGmail(posts);
+    });
+
 }
 
 run();
