58eac897c208bd917e8f60fa88dcdccff0834348
Alex Wu <alex@blitzm.com>
2020-08-28 15:14:35 +1000

    modify scheduler

--- src/app.js
+++ src/app.js
@@ -1,22 +1,21 @@
 require('dotenv').config();
 const crawler = require('./crawler');
 const emailer = require('./emailer');
-const schedule = require('node-schedule');
+var schedule = require('node-schedule-tz');
 
 async function run() {
     
     // run at 6 am every day 
-    schedule.scheduleJob('* * 6 * * *', async function(){
+    schedule.scheduleJob('send at morning', '0 0 6 * * *', 'Australia/Melbourne', async function(){
         const posts = await crawler.getNewPosts();
         emailer.sendEmailByGmail(posts);
     });
 
     // run at 6 pm every day 
-    schedule.scheduleJob('* * 18 * * *', async function(){
+    schedule.scheduleJob('send at evening', '0 0 18 * * *', 'Australia/Melbourne', async function(){
         const posts = await crawler.getNewPosts();
         emailer.sendEmailByGmail(posts);
     });
-
 }
 
 run();
